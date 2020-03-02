41e67e8d4f9b027e48eb20ea0c477265fa0ef1e3
Juliya Yuvchenko <Julia>
2020-03-02 13:27:08 +0200

    delete all not required files

--- src/main/java/com/softserve/dao/interfaces/Read.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.dao.interfaces;
-
-import java.util.Optional;
-import java.util.Set;
-
-public interface Read<T> {
-    Set<T> finalAll();
-    Optional<T> findById(long id);
-}
